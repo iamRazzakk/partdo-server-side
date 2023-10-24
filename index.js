b9fbcddb8a86097bd712abbf7040cfa6b11b7f8f
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-10-24 17:07:08 +0600

    working in deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001;
 // middle ware
 app.use(cors())
 app.use(express.json())
